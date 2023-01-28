f24e679db8a6af934a4a3bfbacd51a2615867662
Luis Cerda <lgcerda@gmail.com>
2023-01-28 02:48:29 -0300

    Terraform Lab AKS

--- AKS/provider.tf
+++ AKS/provider.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
-    subscription_id = "0b71cf90-7326-493e-bddd-19e965b3ae39"
-    client_id = "1b34968c-940a-4cb0-848d-51dbf89b841c"
-    client_secret =  "mdP8Q~rFc71-kXVHjfMSZ5JhuoUVn1MJeuWGOch~"
-    tenant_id = "85047b99-9a7e-4c8e-8950-a89b79c08796"
+    subscription_id = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    client_id = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    client_secret =  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    tenant_id = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
     features {  
     }
 }
--- AKS/terraform.tfvars
+++ AKS/terraform.tfvars
@@ -1,2 +1,2 @@
-aks_service_principal_client_id = "1b34968c-940a-4cb0-848d-51dbf89b841c"
-aks_service_principal_client_secret = "mdP8Q~rFc71-kXVHjfMSZ5JhuoUVn1MJeuWGOch~"
+aks_service_principal_client_id = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+aks_service_principal_client_secret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
